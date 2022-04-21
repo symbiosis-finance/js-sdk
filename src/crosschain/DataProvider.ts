a55da037b3f886cfbd6e35fa268b92afa745ccca
Vladislav Shabanov <vshabanov88@gmail.com>
2022-04-21 20:53:35 +0700

    Cahce

--- src/crosschain/DataProvider.ts
+++ src/crosschain/DataProvider.ts
@@ -4,21 +4,44 @@ import { Symbiosis } from './symbiosis'
 import { UniLikeTrade } from './uniLikeTrade'
 
 export class DataProvider {
+    private cache = new Map<string, any>()
+
     constructor(private readonly symbiosis: Symbiosis) {}
 
-    getRepresentation(token: Token, chainId: ChainId) {
-        return this.symbiosis.getRepresentation(token, chainId)
+    async getRepresentation(token: Token, chainId: ChainId) {
+        return this.fromCache(['getRepresentation', token.address, chainId], () =>
+            this.symbiosis.getRepresentation(token, chainId)
+        )
     }
 
-    getTokenIndex(tokenIn: Token, tokenOut: Token, address: string) {
-        const nervePool = this.symbiosis.nervePool(tokenIn, tokenOut)
+    async getTokenIndex(tokenIn: Token, tokenOut: Token, address: string) {
+        return this.fromCache(['getTokenIndex', tokenIn.address, tokenIn.address, address], () => {
+            const nervePool = this.symbiosis.nervePool(tokenIn, tokenOut)
 
-        return nervePool.getTokenIndex(address)
+            return nervePool.getTokenIndex(address)
+        })
     }
 
-    getPairs(tokenIn: Token, tokenOut: Token) {
-        const provider = this.symbiosis.getProvider(tokenIn.chainId)
+    async getPairs(tokenIn: Token, tokenOut: Token) {
+        return this.fromCache(['getPairs', tokenIn.address, tokenIn.address], () => {
+            const provider = this.symbiosis.getProvider(tokenIn.chainId)
 
-        return UniLikeTrade.getPairs(provider, tokenIn, tokenOut)
+            return UniLikeTrade.getPairs(provider, tokenIn, tokenOut)
+        })
+    }
+
+    private async fromCache<T>(key: (number | string)[], func: () => Promise<T>): Promise<T> {
+        const stringKey = key.join('-')
+
+        let value = this.cache.get(stringKey)
+        if (value) {
+            return value
+        }
+
+        value = await func()
+
+        this.cache.set(stringKey, value)
+
+        return value
     }
 }
