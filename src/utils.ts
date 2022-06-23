2de72390912cffda8277b7e41c9cf7c1aad5cbe2
Alexey Lushnikov <al.lushnikov@yandex.ru>
2022-06-23 09:45:13 +0300

    do not warn not checksummed address

--- src/utils.ts
+++ src/utils.ts
@@ -18,7 +18,7 @@ export function validateAndParseAddress(address: string): string {
 
     try {
         const checksummedAddress = getAddress(address)
-        warning(address === checksummedAddress, `${address} is not checksummed.`)
+        // warning(address === checksummedAddress, `${address} is not checksummed.`)
         return checksummedAddress
     } catch (error) {
         invariant(false, `${address} is not a valid address.`)
