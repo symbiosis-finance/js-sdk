624295d7c71a7e55943c2a494b8a4bcfb3399a09
Vladislav Shabanov <vshabanov88@gmail.com>
2022-08-09 23:07:44 +0700

    Beefy

--- src/crosschain/symbiosis.ts
+++ src/crosschain/symbiosis.ts
@@ -12,6 +12,8 @@ import {
     AdaRouter__factory,
     AvaxRouter,
     AvaxRouter__factory,
+    BeefyVault,
+    BeefyVault__factory,
     BenqiQiErc20,
     BenqiQiErc20__factory,
     Bridge,
@@ -55,6 +57,7 @@ import { ZappingRenBTC } from './zappingRenBTC'
 
 import { config as mainnet } from './config/mainnet'
 import { config as testnet } from './config/testnet'
+import { ZappingBeefy } from './zappingBeefy'
 
 type ConfigName = 'testnet' | 'mainnet'
 
@@ -131,6 +134,10 @@ export class Symbiosis {
 
     public newZappingRenBTC() {
         return new ZappingRenBTC(this)
+    }
+
+    public newZappingBeefy() {
+        return new ZappingBeefy(this)
     }
 
     public getPendingRequests(address: string): Promise<PendingRequest[]> {
@@ -301,6 +308,12 @@ export class Symbiosis {
         const signerOrProvider = signer || this.getProvider(chainId)
 
         return RenMintGatewayV3__factory.connect(address, signerOrProvider)
+    }
+
+    public beefyVault(address: string, chainId: ChainId, signer?: Signer): BeefyVault {
+        const signerOrProvider = signer || this.getProvider(chainId)
+
+        return BeefyVault__factory.connect(address, signerOrProvider)
     }
 
     public stables(): Token[] {
---
+++ src/crosschain/zappingBeefy.ts
@@ -0,0 +1,117 @@
+import { Contract } from '@ethersproject/contracts'
+import { ChainId } from 'src/constants'
+import ERC20 from '../abis/ERC20.json'
+import { Token, TokenAmount, wrappedToken } from '../entities'
+import { SwapExactIn, BaseSwapping } from './baseSwapping'
+import { BeefyVault, MulticallRouter } from './contracts'
+
+export class ZappingBeefy extends BaseSwapping {
+    protected multicallRouter!: MulticallRouter
+    protected userAddress!: string
+    protected beefyVault!: BeefyVault
+    protected aToken!: string
+
+    public async exactIn(
+        tokenAmountIn: TokenAmount,
+        vaultAddress: string,
+        vaultChainId: ChainId,
+        from: string,
+        to: string,
+        revertableAddress: string,
+        slippage: number,
+        deadline: number,
+        use1Inch = true
+    ): SwapExactIn {
+        this.multicallRouter = this.symbiosis.multicallRouter(vaultChainId)
+        this.userAddress = to
+
+        console.log(vaultAddress, vaultChainId)
+        this.beefyVault = this.symbiosis.beefyVault(vaultAddress, vaultChainId)
+
+        const tokenAddress = await this.beefyVault.want()
+        const tokenContract = new Contract(tokenAddress, ERC20, this.symbiosis.providers.get(vaultChainId))
+        const decimals = await tokenContract.decimals()
+
+        const token = new Token({
+            address: tokenAddress,
+            chainId: vaultChainId,
+            decimals,
+        })
+
+        return this.doExactIn(
+            tokenAmountIn,
+            token,
+            from,
+            this.multicallRouter.address,
+            revertableAddress,
+            slippage,
+            deadline,
+            use1Inch
+        )
+    }
+
+    protected finalReceiveSide(): string {
+        return this.multicallRouter.address
+    }
+
+    protected finalCalldata(): string | [] {
+        return this.buildMulticall()
+    }
+
+    protected finalOffset(): number {
+        return 36
+    }
+
+    protected swapTokens(): string[] {
+        const tokens = this.transit.route.map((i) => i.address)
+        if (this.tradeC) {
+            tokens.push(wrappedToken(this.tradeC.amountOut.token).address)
+        } else {
+            tokens.push(this.aToken)
+        }
+        return tokens
+    }
+
+    private buildMulticall() {
+        const callDatas = []
+        const receiveSides = []
+        const path = []
+        const offsets = []
+
+        let amount
+        let supplyToken
+
+        if (this.tradeC) {
+            amount = this.tradeC.tokenAmountIn.raw.toString()
+            supplyToken = this.tradeC.amountOut.token
+
+            callDatas.push(this.tradeC.callData)
+            receiveSides.push(this.tradeC.routerAddress)
+            path.push(this.tradeC.tokenAmountIn.token.address)
+            offsets.push(this.tradeC.callDataOffset!)
+        } else {
+            amount = this.transit.amountOut.raw.toString()
+            if (this.transit.direction === 'mint') {
+                supplyToken = this.transit.amountOut.token
+            } else {
+                supplyToken = this.transit.feeToken
+            }
+        }
+
+        const beefyCalldata = this.beefyVault.interface.encodeFunctionData('deposit', ['0']) // amount will be patched
+
+        callDatas.push(beefyCalldata)
+        receiveSides.push(this.beefyVault.address)
+        path.push(supplyToken.address, this.beefyVault.address)
+        offsets.push(36)
+
+        return this.multicallRouter.interface.encodeFunctionData('multicall', [
+            amount,
+            callDatas,
+            receiveSides,
+            path,
+            offsets,
+            this.userAddress,
+        ])
+    }
+}
