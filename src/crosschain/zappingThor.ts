5cafba1bb4a2235c0c119e29ad8479a7f31f226f
allush <al.lushnikov@yandex.ru>
2024-01-23 11:25:56 +0000

    zero affiliate fee

--- src/crosschain/zappingThor.ts
+++ src/crosschain/zappingThor.ts
@@ -231,7 +231,7 @@ export class ZappingThor extends BaseSwapping {
         url.searchParams.set('destination', this.bitcoinAddress)
         url.searchParams.set('streaming_interval', '1')
         url.searchParams.set('affiliate', 'sy')
-        url.searchParams.set('affiliate_bps', '30')
+        url.searchParams.set('affiliate_bps', '0')
         // url.searchParams.set('tolerance_bps', '300') // 3% FIXME
 
         const response = await fetch(url.toString())
