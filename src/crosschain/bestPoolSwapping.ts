93c4a70cda489baf23d20369259e7808bafedd7f
allush <al.lushnikov@yandex.ru>
2024-04-28 07:09:19 +0100

    add direct route clients list

--- src/crosschain/bestPoolSwapping.ts
+++ src/crosschain/bestPoolSwapping.ts
@@ -4,8 +4,13 @@ import type { Swapping } from './swapping'
 import type { Symbiosis } from './symbiosis'
 import type { OmniPoolConfig } from './types'
 import { Error, ErrorCode } from './error'
+import {utils} from "ethers";
 
 type WaitForCompleteArgs = Parameters<typeof Swapping.prototype.waitForComplete>
+
+const DIRECT_ROUTE_CLIENTS = [
+    utils.formatBytes32String('lifi')
+]
 
 // Swapping wrapper what select best omni pool for swapping
 export class BestPoolSwapping {
@@ -38,9 +43,11 @@ export class BestPoolSwapping {
                 const action = this.symbiosis.newSwapping(optimalOmniPool)
                 const actionResult = await action.exactIn(exactInParams)
 
-                const priceImpactThreshold = new Percent('-5', '1000') // -0.5%
-                if (actionResult.priceImpact.lessThan(priceImpactThreshold)) {
-                    throw new Error('Price impact of optimal octopool is too high')
+                if (!DIRECT_ROUTE_CLIENTS.includes(this.symbiosis.clientId)) {
+                    const priceImpactThreshold = new Percent('-5', '1000') // -0.5%
+                    if (actionResult.priceImpact.lessThan(priceImpactThreshold)) {
+                        throw new Error('Price impact of optimal octopool is too high')
+                    }
                 }
 
                 this.swapping = action
