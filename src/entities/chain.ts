d1a48931c3b8f9ce321dbef167735783f0eda187
allush <al.lushnikov@yandex.ru>
2024-06-04 19:51:20 +0100

    update rootstock icon

--- src/entities/chain.ts
+++ src/entities/chain.ts
@@ -512,8 +512,8 @@ export const chains: Chain[] = [
         disabled: false,
         explorer: 'https://rootstock.blockscout.com',
         icons: {
-            small: 'https://rootstock.blockscout.com/favicon/favicon-32x32.png',
-            large: 'https://rootstock.blockscout.com/favicon/favicon-32x32.png',
+            small: 'https://rootstock.blockscout.com/assets/favicon/favicon-32x32.png',
+            large: 'https://rootstock.blockscout.com/assets/favicon/favicon-32x32.png',
         },
     }),
     new Chain({
