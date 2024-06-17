b78cb41386388c955511fb363ade53007a41c695
allush <al.lushnikov@yandex.ru>
2024-06-17 13:43:54 +0100

    upd

--- src/crosschain/zappingBtc.ts
+++ src/crosschain/zappingBtc.ts
@@ -4,7 +4,9 @@ import { BaseSwapping, CrosschainSwapExactInResult } from './baseSwapping'
 import { MulticallRouter, Synthesis } from './contracts'
 import { OneInchProtocols } from './trade/oneInchTrade'
 import { Network, networks, address, initEccLib } from 'bitcoinjs-lib'
-import * as ecc from 'tiny-secp256k1'
+import ecc from '@bitcoinerlab/secp256k1'
+
+initEccLib(ecc)
 
 export interface ZappingThorExactInParams {
     tokenAmountIn: TokenAmount
@@ -22,14 +24,10 @@ export const BTC_NETWORKS: Partial<Record<ChainId, Network>> = {
 }
 
 export function getPkScript(addr: string, btcChain: Network): Buffer {
-    initEccLib(ecc)
-
     return address.toOutputScript(addr, btcChain)
 }
 
 export function getAddress(pkScript: string, btcChain: Network): string {
-    initEccLib(ecc)
-
     return address.fromOutputScript(Buffer.from(pkScript.substring(2), 'hex'), btcChain)
 }
 
