f5f5a15ac48063560a78a0fda018e12f33ebc5a9
allush <al.lushnikov@yandex.ru>
2024-11-29 14:15:41 +0000

    pure octopool onchain swap

---
+++ src/crosschain/swapExactIn/octoPoolSwap.ts
@@ -0,0 +1,68 @@
+import { SwapExactInParams, SwapExactInResult } from '../types'
+import { OctoPoolTrade } from '../trade'
+
+export function isOctoPoolSwapSupported(context: SwapExactInParams): boolean {
+    const { tokenAmountIn, tokenOut, symbiosis } = context
+
+    if (!tokenAmountIn.token.isSynthetic || !tokenOut.isSynthetic) {
+        return false
+    }
+
+    const tokenInPool = symbiosis.getOmniPoolByToken(tokenAmountIn.token)
+    const tokenOutPool = symbiosis.getOmniPoolByToken(tokenOut)
+    if (!tokenInPool || !tokenOutPool) {
+        return false
+    }
+
+    return tokenInPool.id === tokenOutPool.id
+}
+
+export async function octoPoolSwap({
+    symbiosis,
+    tokenAmountIn,
+    tokenOut,
+    to,
+    slippage,
+    deadline,
+}: SwapExactInParams): Promise<SwapExactInResult> {
+    debugger
+    const tokenInPool = symbiosis.getOmniPoolByToken(tokenAmountIn.token)
+    const tokenOutPool = symbiosis.getOmniPoolByToken(tokenOut)
+    if (!tokenInPool || !tokenOutPool) {
+        throw new Error('Incorrect tokens for octoPoolSwap')
+    }
+
+    const trade = new OctoPoolTrade({
+        symbiosis,
+        tokenAmountIn,
+        tokenAmountInMin: tokenAmountIn,
+        tokenOut,
+        omniPoolConfig: tokenInPool,
+        to,
+        slippage,
+        deadline,
+    })
+
+    await trade.init()
+
+    return {
+        kind: 'onchain-swap',
+        tokenAmountOut: trade.amountOut,
+        tokenAmountOutMin: trade.amountOutMin,
+        approveTo: trade.routerAddress,
+        priceImpact: trade.priceImpact,
+        transactionType: 'evm',
+        transactionRequest: {
+            to: trade.routerAddress,
+            data: trade.callData,
+            value: tokenAmountIn.token.isNative ? tokenAmountIn.raw.toString() : undefined,
+        },
+        fees: [],
+        routes: [
+            {
+                provider: 'octopool',
+                tokens: [tokenAmountIn.token, tokenOut],
+            },
+        ],
+    }
+}
--- src/crosschain/swapExactIn/onchainSwap.ts
+++ src/crosschain/swapExactIn/onchainSwap.ts
@@ -3,12 +3,16 @@ import { SwapExactInParams, SwapExactInResult } from '../types'
 import { MagpieTrade } from '../trade/magpieTrade'
 import { magpieSwap } from './magpieSwap'
 import { theBestOutput } from './utils'
+import { isOctoPoolSwapSupported, octoPoolSwap } from './octoPoolSwap'
 
 export async function onchainSwap(params: SwapExactInParams): Promise<SwapExactInResult> {
     const promises: Promise<SwapExactInResult>[] = [aggregatorsSwap(params)]
 
     if (MagpieTrade.isAvailable(params.tokenAmountIn.token.chainId)) {
         promises.push(magpieSwap(params))
+    }
+    if (isOctoPoolSwapSupported(params)) {
+        promises.push(octoPoolSwap(params))
     }
 
     return theBestOutput(promises)
