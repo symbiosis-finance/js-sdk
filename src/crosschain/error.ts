06b6e331041c3d1f631b2944aedf91e6a97491c1
Nick <nick.medvik@gmail.com>
2025-08-11 17:30:16 +0300

    add error liquidity and remove extra labels for sdk histogram

--- src/crosschain/error.ts
+++ src/crosschain/error.ts
@@ -25,6 +25,7 @@ export class Error {
 enum SwapAggregatorErrorCategory {
     RateLimit = 'rate_limit',
     SwapAggregatorError = 'swap_aggregator_error',
+    LiquidityError = 'liquidity_error',
     TokenError = 'token_error',
     BigIntError = 'bigint_error',
     Unknown = 'unknown',
@@ -33,14 +34,12 @@ enum SwapAggregatorErrorCategory {
 const aggregatorCategoryPatterns: Record<SwapAggregatorErrorCategory, (string | RegExp)[]> = {
     [SwapAggregatorErrorCategory.RateLimit]: ['rate limit', 'too many requests', 'the limit of requests', /\b429\b/],
     [SwapAggregatorErrorCategory.SwapAggregatorError]: [
-        'swap error',
-        'cannot create trade',
-        'insufficient liquidity',
-        'no path found',
-        'route not found',
-        'No avail liquidity',
-        'liquidity',
+        'swap error', // 1inch
+        'cannot create trade', // uni v2
+        'no path found', // izumi
+        'route not found', // uni v3
     ],
+    [SwapAggregatorErrorCategory.LiquidityError]: ['insufficient liquidity', 'No avail liquidity'],
     [SwapAggregatorErrorCategory.BigIntError]: ['converted to bigint', 'to a bigint'],
     [SwapAggregatorErrorCategory.TokenError]: ['not valid token'],
     [SwapAggregatorErrorCategory.Unknown]: ['unknown'],
