762cf6fa90c73a0d24c174e5d9646433cdec791b
allush <al.lushnikov@yandex.ru>
2026-05-14 08:15:55 +0100

    deprecate usdc.n on sei

--- src/crosschain/config/mainnet.ts
+++ src/crosschain/config/mainnet.ts
@@ -1671,7 +1671,7 @@ export const config: Config = {
                     },
                 },
                 {
-                    // deprecated: true,
+                    deprecated: true,
                     name: 'USD Coin via Noble',
                     symbol: 'USDC.n',
                     address: '0x3894085Ef7Ff0f0aeDf52E2A2704928d1Ec074F1',
